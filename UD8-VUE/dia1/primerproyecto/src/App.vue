c0ee72343e394a81cc9a58a836ddafdc9027394c
arodriguezgim <arodriguezg@iesch.org>
2025-05-13 21:20:47 +0200

    Fin clase

--- UD8-VUE/dia1/primerproyecto/src/App.vue
+++ UD8-VUE/dia1/primerproyecto/src/App.vue
@@ -1,92 +1,37 @@
 <script setup>
-  const nombre = "Alberto";
-  const color = "color:red";
-  const esMartes = null;
-  const dias = ["Lunes", "Martes", "Miercoles"];
-  const activo = false;
-  //const arrayFrutas = ["🍎", "🍌", "🍉", "🍓", "🍒"];
-  const arrayFrutas = [
-    {
-      name: "Manzana",
-      price: "3.00€",
-      description: "Manzana Golden",
-      stock: 0,
-    },
-    {
-      name: "Limón",
-      price: "2.00€",
-      description: "limon",
-      stock: 5,
-    },
-    {
-      name: "Naranja",
-      price: "1.00€",
-      description: "Naranja de zumo",
-      stock: 2,
-    },
-  ];
+  import { ref, computed } from 'vue';
 
-  const objetoFruta = {
-    name: "Naranja",
-    price: "1.00€",
-    description: "Naranja de zumo"
-  }
+  const contador = ref(0);
 
-  const hacerClick = ( mensaje ) => {
-    console.log( mensaje );
+  const sumar = () => {
+    contador.value++;
+
+    console.log( contador.value );
   }
 </script>
 
 <template>
-  <h1 :style="color">Hola {{ nombre.toLowerCase() }} !!</h1>
-  <!--
-  <h2> {{ esMartes ? "Hoy es Martes" : "Hoy NO es Martes" }}</h2>
-  <h2> {{  dias[2]  }}</h2>
+  <div class="container">
+    <h1> Variables reactivas</h1>
+    <h2 :class=" contador.value > 5 ? 'verde' : 'rojo'"> {{  contador }}</h2>
+    <div class="container">
+      <button class="btn btn-primary m-2" @click="sumar()">Incrementar</button>
+      <button class="btn btn-warning m-2" @click="sumar()">Resetear</button>
+      <button class="btn btn-danger m-2" @click="sumar()">Restar</button>
+    </div>
+    
+  </div>
   
-
-  <h2 v-if="esMartes === true">Es MARTES</h2>
-  <h2 v-else-if="esMartes === false">NO es martes</h2>
-  <h2 v-else>No tengo NI IDEA de Qué día es</h2>
-
-  <h2 v-show="activo">Estoy en true</h2>
-
-
-  <ul>
-    <li v-for="fruta in arrayFrutas" :key="name">
-      {{ fruta.name }} - {{  fruta.price }} - {{  fruta.description }}
-    </li>
-
-  </ul>
-
-  <h2>Objetos</h2>
-
-  <ul>
-    <li v-for="(valor, propiedad, index) in objetoFruta">
-      {{ propiedad }} : {{ valor }} : {{ index }}
-    </li>
-  </ul>
-  -->
-  <h2>V-if con V-for</h2>
-  <template 
-      v-for="fruta in arrayFrutas"
-      :key="fruta.name">
-    <ul>
-      <li v-if="fruta.stock > 0"> {{  fruta.name }}</li>
-    </ul>
-  </template>
-
-  <button @click.left="hacerClick('Click en Boton 1')">Boton Izquierdo</button>
-  <button @click.middle="hacerClick('Click en Boton 2')">Boton Centro</button>
-  <button @click.right.prevent="hacerClick('Click en Boton 2')">Boton Derecho</button>
-
-
-
-
-
-
 </template>
 
 <style>
+
+  .rojo {
+    color: red;
+  }
+  .verde {
+    color: green;
+  }
 
 </style>
  
