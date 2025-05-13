a4fcb5971b84282cab0d60aa8b77f6d7d0ffa997
arodriguezgim <arodriguezg@iesch.org>
2025-05-13 20:26:45 +0200

    Fin primera hora

--- UD8-VUE/dia1/primerproyecto/src/App.vue
+++ UD8-VUE/dia1/primerproyecto/src/App.vue
@@ -4,7 +4,37 @@
   const esMartes = null;
   const dias = ["Lunes", "Martes", "Miercoles"];
   const activo = false;
-  const arrayFrutas = ["🍎", "🍌", "🍉", "🍓", "🍒"];
+  //const arrayFrutas = ["🍎", "🍌", "🍉", "🍓", "🍒"];
+  const arrayFrutas = [
+    {
+      name: "Manzana",
+      price: "3.00€",
+      description: "Manzana Golden",
+      stock: 0,
+    },
+    {
+      name: "Limón",
+      price: "2.00€",
+      description: "limon",
+      stock: 5,
+    },
+    {
+      name: "Naranja",
+      price: "1.00€",
+      description: "Naranja de zumo",
+      stock: 2,
+    },
+  ];
+
+  const objetoFruta = {
+    name: "Naranja",
+    price: "1.00€",
+    description: "Naranja de zumo"
+  }
+
+  const hacerClick = () => {
+    console.log("Me has hecho click!!");
+  }
 </script>
 
 <template>
@@ -19,13 +49,40 @@
   <h2 v-else>No tengo NI IDEA de Qué día es</h2>
 
   <h2 v-show="activo">Estoy en true</h2>
-  -->
+
+
   <ul>
-    <li v-for="(fruta, index) in arrayFrutas" :key="index" >
-      {{ index }} - {{ fruta }}
+    <li v-for="fruta in arrayFrutas" :key="name">
+      {{ fruta.name }} - {{  fruta.price }} - {{  fruta.description }}
     </li>
 
   </ul>
+
+  <h2>Objetos</h2>
+
+  <ul>
+    <li v-for="(valor, propiedad, index) in objetoFruta">
+      {{ propiedad }} : {{ valor }} : {{ index }}
+    </li>
+  </ul>
+  -->
+  <h2>V-if con V-for</h2>
+  <template 
+      v-for="fruta in arrayFrutas"
+      :key="fruta.name">
+    <ul>
+      <li v-if="fruta.stock > 0"> {{  fruta.name }}</li>
+    </ul>
+  </template>
+
+  <button v-on:click="hacerClick()">Hazme click!!!</button>
+  <button @click="hacerClick()">Hazme click 2!!!</button>
+
+
+
+
+
+
 </template>
 
 <style>
