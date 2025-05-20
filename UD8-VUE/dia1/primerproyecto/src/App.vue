2a7ca273521484c48e115dcecbceff635ceca565
arodriguezgim <arodriguezg@iesch.org>
2025-05-20 19:36:54 +0200

    Fin clase 1

--- UD8-VUE/dia1/primerproyecto/src/App.vue
+++ UD8-VUE/dia1/primerproyecto/src/App.vue
@@ -3,21 +3,51 @@
 
   const contador = ref(0);
 
+  const arrayFavoritos = ref([]);
+
+  const claseContador = computed( () => {
+
+    if (contador.value === 0) {
+      return "zero";
+    }
+    return contador.value > 0 ? "verde" : "rojo";
+  })
+
+  const bloquearBoton = computed( () =>{
+
+    const numero = arrayFavoritos.value.find( (num) => num === contador.value );
+    console.log(numero)
+    if (contador.value === 0) return true;
+    return false;
+
+    //return numero || numero === 0;
+  })
+
   const sumar = () => {
     contador.value++;
-
-    console.log( contador.value );
+  }
+  const resetear = () => {
+    contador.value = 0;
+  }
+  const restar = () => {
+    contador.value--;
+  }
+  const anadir = () => {
+    arrayFavoritos.value.push( contador.value );
   }
 </script>
 
 <template>
   <div class="container">
     <h1> Variables reactivas</h1>
-    <h2 :class=" contador.value > 5 ? 'verde' : 'rojo'"> {{  contador }}</h2>
+    <h2 :class="claseContador"> {{  contador }}</h2>
     <div class="container">
       <button class="btn btn-primary m-2" @click="sumar()">Incrementar</button>
-      <button class="btn btn-warning m-2" @click="sumar()">Resetear</button>
-      <button class="btn btn-danger m-2" @click="sumar()">Restar</button>
+      <button class="btn btn-warning m-2" @click="resetear()">Resetear</button>
+      <button class="btn btn-danger m-2" @click="restar()">Restar</button>
+      <button class="btn btn-success m-2" @click="anadir()" :disabled="bloquearBoton">Añadir Favorito</button>
+      <br>
+      {{ arrayFavoritos }}
     </div>
     
   </div>
@@ -31,6 +61,9 @@
   }
   .verde {
     color: green;
+  }
+  .zero {
+    color: black;
   }
 
 </style>
